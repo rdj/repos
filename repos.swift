4a0fa48e0301d66ac3f89e32de5b5fd05e0d89e4
Ryan D Johnson <ryandjohnson@gmail.com>
2015-01-23 14:01:39 -0800

    Working repos script in swift

--- repos.swift
+++ repos.swift
@@ -2,6 +2,66 @@
 
 import AppKit
 
+let screenSize = NSScreen.mainScreen()!.frame.size
+
+let procsOfInterest = [
+    "1Password 4",
+    "Google Chrome",
+    "Chromium",
+    "EchofonLite",
+    "iCal",
+    "GitX",
+    "Calendar",
+    "HipChat",
+    "iTerm",
+    "iTunes",
+    "Mail",
+    "Mailplane 3",
+    "Messages",
+    "Safari",
+    "Things",
+    "Terminal",
+    "Xcode",
+];
+
+let MENU_BAR_HEIGHT = 22
+let DOCK_HEIGHT = 64
+
+func center_horizontally( dw:Int, ww:Int ) -> Int {
+    return ( dw - ww ) / 2
+}
+
+func maxheight( dh:Int ) -> Int {
+    return dh - MENU_BAR_HEIGHT - DOCK_HEIGHT
+}
+
+func center_vertically( dh:Int, wh:Int ) -> Int {
+    let effective_display_height = maxheight( dh )
+    let y_offset_from_menu = ( effective_display_height - wh ) / 2
+    return MENU_BAR_HEIGHT + y_offset_from_menu
+}
+
+func centered( dw:Int, dh:Int, ww:Int, wh:Int ) -> CGRect {
+    let wx = center_horizontally( dw, ww )
+    let wy = center_vertically( dh, wh )
+    return CGRect( x:wx, y:wy, width:ww, height:wh )
+}
+
+var configurationForWidth:[CGFloat:[String:CGRect]] = [
+    2560 : {
+        let dw = 2560, dh = 1440
+        return [
+            "standard"    : centered( dw, dh, 1600, 1200 ),
+            "EchofonLite" : centered( dw, dh,  489, 1080 ),
+            "Things"      : centered( dw, dh,  768,  512 ),
+            "HipChat"     : CGRect( x:0,        y:MENU_BAR_HEIGHT, width:768,  height:maxheight( dh ) ),
+            "iTerm"       : CGRect( x:dw - 554, y:693,             width:554,  height:388 ),
+            "Messages"    : CGRect( x:996,      y:440,             width:768,  height:512 ),
+            "Terminal"    : CGRect( x:694,      y:MENU_BAR_HEIGHT, width:1202, height:1341 ),
+        ]
+    }(),
+];
+
 // The AXUIElement APIs are the assistive / accessibility APIs that do
 // actually let you do stuff like move stuff around on the screen.
 // They are ancient and annoying CF APIs that lack modern annotation
@@ -36,14 +96,34 @@ struct RAXAttributeConstants {
     static let Windows = "AXWindows"
 }
 
-class RAXProcess {
+class RAXWrapper {
     private let handle:AXUIElement
+
+    private init( handle:AXUIElement ) {
+        self.handle = handle
+    }
+
+    var attributeNames:[String] {
+        var out:Unmanaged<CFArray>? = nil
+        var axerr = AXUIElementCopyAttributeNames(
+            handle,
+            &out
+        )
+        if AXError(kAXErrorSuccess) != axerr || nil == out {
+            return [String]()
+        }
+        let names:[AnyObject] = out!.takeRetainedValue()
+        return names.map{ $0 as String }
+    }
+}
+
+class RAXProcess: RAXWrapper {
     private let maxWindows = 20
     let pid:Int
         
     init( _ pid:Int ) {
         self.pid = pid
-        self.handle = AXUIElementCreateApplication( pid_t( self.pid ) ).takeRetainedValue()
+        super.init( handle: AXUIElementCreateApplication( pid_t( self.pid ) ).takeRetainedValue() )
     }
 
     lazy var windows:[RAXWindow] = {
@@ -60,28 +140,13 @@ class RAXProcess {
         }
 
         let handles:[AnyObject] = out!.takeRetainedValue()
-        return handles.map{ RAXWindow( $0 as AXUIElement ) }
+        return handles.map{ RAXWindow( $0 as AXUIElement ) }.filter{ $0.canChange }
     }()
 }
 
-class RAXWindow {
-    private let handle:AXUIElement
-    
+class RAXWindow: RAXWrapper {
     init( _ handle:AXUIElement ) {
-        self.handle = handle
-    }
-
-    var attributeNames:[String] {
-        var out:Unmanaged<CFArray>? = nil
-        var axerr = AXUIElementCopyAttributeNames(
-            handle,
-            &out
-        )
-        if AXError(kAXErrorSuccess) != axerr || nil == out {
-            return [String]()
-        }
-        let names:[AnyObject] = out!.takeRetainedValue()
-        return names.map{ $0 as String }
+        super.init( handle: handle )
     }
 
     var canChange:Bool {
@@ -152,28 +217,59 @@ class RAXWindow {
     }
 }
 
-var apps = NSWorkspace.sharedWorkspace().runningApplications
+func apps() -> [ NSRunningApplication ] {
+    let apps = NSWorkspace.sharedWorkspace().runningApplications as [ NSRunningApplication ]
+    return apps.filter { contains( procsOfInterest, $0.localizedName! ) }
+}
 
-for app in apps {
-    var printed = false
-
-    var proc = RAXProcess( Int(app.processIdentifier) )
-    for w in proc.windows.filter( { $0.canChange } ) {
-        if !printed {
-            println( "\(app.processIdentifier) \(app.localizedName!!)" )
+func query() {
+    var qconf = [String:CGRect]()
+    for app in apps() {
+        let proc = RAXProcess( Int(app.processIdentifier) )
+        if proc.windows.count > 0 {
+            let w = proc.windows[0]
+            qconf[app.localizedName!] = CGRectMake(
+                w.position.x, w.position.y,
+                w.size.width, w.size.height
+            )
         }
+    }
 
-        println( " - (\(Int(w.position.x)), \(Int(w.position.y))) \(Int(w.size.width))x\(Int(w.size.height)) <\(w.title)>" )
-        // if w.canChange {
-        //     w.position = CGPointMake( 480, 99 )
-        //     w.size = CGSizeMake( 1600, 1200 )
-        // }
+    var conf = [CGFloat:[String:CGRect]]()
+    conf[screenSize.width] = qconf
+    println( conf )
+}
+
+func doPositioning() {
+    let conf = configurationForWidth[screenSize.width]!
+    for app in apps() {
+        let rect = conf[app.localizedName!] ?? conf["standard"]!
+        let proc = RAXProcess( Int(app.processIdentifier) )
+        for w in proc.windows {
+            w.position = rect.origin
+            w.size = rect.size
+        }
     }
 }
 
+func fixEmacs() {
+    let task = NSTask()
+    task.launchPath = "/usr/local/bin/emacsclient"
+    task.arguments = [
+        "-e",
+        "(rdj-smartsize-frame-for \(Int(screenSize.width)) \(Int(screenSize.height)))"
+    ]
+    task.launch()
+}
 
+func main( argv:[String] ) {
+    if contains( argv, "-q" ) {
+        query()
+    }
+    else {
+        doPositioning()
+        fixEmacs()
+    }
+}
 
-
-
-
-
+main( Process.arguments )
